cdf31dc60078665785fdf09ad394caec951130eb
Lokey2411 <hahaiviet24112003@gmail.com>
2023-10-26 23:27:08 +0700

    pull data

--- src/constansts/items.ts
+++ src/constansts/items.ts
@@ -5,127 +5,127 @@ import { firestore } from "../firebase";
 
 export const quizzes = [
 	{
-		title: "Scratch là gì?",
+		title: "Roblox Studio là gì?",
 		ans: [
 			{
 				answer: "A",
-				title: "Ngôn ngữ lập trình",
+				title: "Một trò chơi trực tuyến",
 			},
 			{
 				answer: "B",
-				title: "Một loại thức ăn",
+				title: "Một trình soạn thảo game",
 			},
 			{
 				answer: "C",
-				title: "Máy tính",
+				title: "Một mạng xã hội",
 			},
 			{
 				answer: "D",
-				title: "Xe hơi",
+				title: "Một công ty sản xuất",
 			},
 		],
 		order: 1,
-		rightAns: "A",
+		rightAns: "B",
 		userAns: "",
 		id: `quiz${0}`,
 	},
 	{
-		title: "Trong Scratch, các khối lệnh được ghép nối lại với nhau bằng cách nào?",
+		title: "Roblox Studio cho phép người dùng làm gì?",
 		ans: [
 			{
 				answer: "A",
-				title: "Kéo và thả",
+				title: "Chơi trò chơi",
 			},
 			{
 				answer: "B",
-				title: " Nhấp chuột",
+				title: "Soạn thảo trò chơi",
 			},
 			{
 				answer: "C",
-				title: "Gõ vào bàn phím",
+				title: "Xem video trực tuyến",
 			},
 			{
 				answer: "D",
-				title: "Trượt",
+				title: "Mua đồ ảo",
 			},
 		],
 		order: 2,
-		rightAns: "A",
+		rightAns: "B",
 		userAns: "",
 		id: `quiz${1}`,
 	},
 	{
-		title: "Khối lệnh nào được sử dụng để di chuyển một nhân vật trong Scratch?",
+		title: "Ngôn ngữ lập trình nào được sử dụng trong Roblox Studio?",
 		ans: [
 			{
 				answer: "A",
-				title: "Chuyển động",
+				title: "Python",
 			},
 			{
 				answer: "B",
-				title: " Âm thanh",
+				title: "JavaScript",
 			},
 			{
 				answer: "C",
-				title: "Biến số",
+				title: "LUA",
 			},
 			{
 				answer: "D",
-				title: "Lặp lại",
+				title: "Java",
 			},
 		],
 		order: 3,
-		rightAns: "A",
+		rightAns: "C",
 		userAns: "",
 		id: `quiz${2}`,
 	},
 	{
-		title: "Scratch được phát triển bởi ai?",
+		title: "Cửa sổ hiển thị các vật đang có trong bản đồ là gì?",
 		ans: [
 			{
 				answer: "A",
-				title: "Microsoft",
+				title: "Explorer",
 			},
 			{
 				answer: "B",
-				title: "Apple",
+				title: "Properties",
 			},
 			{
 				answer: "C",
-				title: "MIT Media Lab",
+				title: "Toolbox",
 			},
 			{
 				answer: "D",
-				title: "Google",
+				title: "Terrain",
 			},
 		],
 		order: 4,
-		rightAns: "C",
+		rightAns: "A",
 		userAns: "",
 		id: `quiz${3}`,
 	},
 	{
-		title: "Scratch cho phép bạn làm gì với các nhân vật?",
+		title: "Cửa sổ hiển thị thuộc tính của vật thể là gì?",
 		ans: [
 			{
 				answer: "A",
-				title: "Chỉ xem",
+				title: "Explorer",
 			},
 			{
 				answer: "B",
-				title: "Thay đổi màu sắc",
+				title: "Properties",
 			},
 			{
 				answer: "C",
-				title: "Di chuyển và tương tác",
+				title: "Toolbox",
 			},
 			{
 				answer: "D",
-				title: "Đăng lên mạng xã hội",
+				title: "Terrain",
 			},
 		],
 		order: 5,
-		rightAns: "C",
+		rightAns: "B",
 		userAns: "",
 		id: `quiz${4}`,
 	},
@@ -133,23 +133,23 @@ export const quizzes = [
 
 export const quizzes2 = [
 	{
-		title: "Trong Scratch, các khối lệnh được ghép nối lại với nhau bằng gì?",
+		title: "Thuộc tính Position của một vật thể trong Roblox thể hiện điều gì?",
 		ans: [
 			{
 				answer: "A",
-				title: "Dấu chấm và dấu phẩy",
+				title: "Xác định tên vật thể",
 			},
 			{
 				answer: "B",
-				title: "Dấu gạch nối",
+				title: "Xác định vị trí của vật thể trong thế giới 3D",
 			},
 			{
 				answer: "C",
-				title: "Dấu cách",
+				title: "Xác định màu sắc của vật thể",
 			},
 			{
 				answer: "D",
-				title: " Dấu gạch chéo",
+				title: " Xác định kích thước của vật thể",
 			},
 		],
 		order: 1,
@@ -158,73 +158,73 @@ export const quizzes2 = [
 		id: `quiz${0}`,
 	},
 	{
-		title: "Trong Scratch, khối lệnh nào được sử dụng để tạo một vòng lặp?",
+		title: "Thuộc tính Size của một vật thể trong Roblox thể hiện điều gì?",
 		ans: [
 			{
 				answer: "A",
-				title: "Kết thúc",
+				title: "Xác định tên vật thể",
 			},
 			{
 				answer: "B",
-				title: "Lặp lại",
+				title: "Xác định vị trí của vật thể trong thế giới 3D",
 			},
 			{
 				answer: "C",
-				title: "Điều kiện",
+				title: "Xác định màu sắc của vật thể",
 			},
 			{
 				answer: "D",
-				title: "Chuyển đổi",
+				title: " Xác định kích thước của vật thể",
 			},
 		],
 		order: 2,
-		rightAns: "B",
+		rightAns: "D",
 		userAns: "",
 		id: `quiz${1}`,
 	},
 	{
-		title: "Khi bạn muốn vật thể di chuyển đến một vị trí cụ thể trên sân chơi trong Scratch, bạn sử dụng khối lệnh nào?",
+		title: "Thuộc tính Color của một vật thể trong Roblox thể hiện điều gì?",
 		ans: [
 			{
 				answer: "A",
-				title: "Chuyển đến",
+				title: "Xác định tên vật thể",
 			},
 			{
 				answer: "B",
-				title: "Di chuyển",
+				title: "Xác định vị trí của vật thể trong thế giới 3D",
 			},
 			{
 				answer: "C",
-				title: "Nhảy",
+				title: "Xác định màu sắc của vật thể",
 			},
 			{
 				answer: "D",
-				title: "Quay lại vị trí ban đầu",
+				title: " Xác định kích thước của vật thể",
 			},
 		],
 		order: 3,
-		rightAns: "A",
+		rightAns: "C",
 		userAns: "",
 		id: `quiz${2}`,
 	},
 	{
-		title: "Trong Scratch, bạn có thể tạo biến bằng cách nào?",
+		title: "Thuộc tính Transparency của một vật thể trong Roblox thể hiện điều gì?",
 		ans: [
 			{
 				answer: "A",
-				title: "Tạo biến",
+				title: "Xác định độ trong suốt của vật thể",
 			},
 			{
 				answer: "B",
-				title: "Thay đổi biến",
+				title: "Xác định vị trí của vật thể trong thế giới 3D",
 			},
 			{
 				answer: "C",
-				title: "Lặp lại",
+				title: "Xác định màu sắc của vật thể",
 			},
 			{
 				answer: "D",
-				title: "Nếu...thì",
+				title: " Xác định kích thước của vật thể",
 			},
 		],
 		order: 4,
@@ -233,23 +233,151 @@ export const quizzes2 = [
 		id: `quiz${3}`,
 	},
 	{
-		title: "Trong Scratch, để làm cho một vật thể bắt đầu quay khi bạn nhấn vào nó, bạn sử dụng khối lệnh nào?",
+		title: "Thuộc tính Reflectance của một vật thể trong Roblox thể hiện điều gì?",
 		ans: [
 			{
 				answer: "A",
-				title: "Thay đổi hướng",
+				title: "Xác định tên vật thể",
 			},
 			{
 				answer: "B",
-				title: "Bắt đầu quay",
+				title: "Xác định vị trí của vật thể trong thế giới 3D",
 			},
 			{
 				answer: "C",
-				title: "Kết thúc quay",
+				title: "Xác định màu sắc của vật thể",
 			},
 			{
 				answer: "D",
-				title: "Chuyển đến",
+				title: " Xác định độ phản chiếu của vật thể",
+			},
+		],
+		order: 5,
+		rightAns: "D",
+		userAns: "",
+		id: `quiz${4}`,
+	},
+];
+
+export const quizzes3 = [
+	{
+		title: "Solid Modeling là gì?",
+		ans: [
+			{
+				answer: "A",
+				title: "Một phương pháp tạo hình 3D bằng việc kết hợp các đối tượng 2D",
+			},
+			{
+				answer: "B",
+				title: "Một phương pháp tạo hình 3D bằng việc sử dụng chỉ vẽ 3D",
+			},
+			{
+				answer: "C",
+				title: "Một phương pháp tạo hình 3D bằng việc kết hợp các đối tượng 3D",
+			},
+			{
+				answer: "D",
+				title: "Một phương pháp tạo hình 3D bằng việc thảo luận với người khác",
+			},
+		],
+		order: 1,
+		rightAns: "C",
+		userAns: "",
+		id: `quiz${0}`,
+	},
+	{
+		title: "Chức năng Union có tác dụng gì?",
+		ans: [
+			{
+				answer: "A",
+				title: "Gộp nhiều đối tượng lại với nhau",
+			},
+			{
+				answer: "B",
+				title: "Tạo ra màu sắc",
+			},
+			{
+				answer: "C",
+				title: "Xoay đối tượng quanh trục",
+			},
+			{
+				answer: "D",
+				title: "Chia tách đối tượng",
+			},
+		],
+		order: 2,
+		rightAns: "A",
+		userAns: "",
+		id: `quiz${1}`,
+	},
+	{
+		title: "Chức năng Subtract có tác dụng gì?",
+		ans: [
+			{
+				answer: "A",
+				title: "Gộp nhiều đối tượng lại với nhau",
+			},
+			{
+				answer: "B",
+				title: "Tạo ra màu sắc",
+			},
+			{
+				answer: "C",
+				title: "Phép cắt bớt một đối tượng khỏi một đối tượng khác",
+			},
+			{
+				answer: "D",
+				title: "Chia tách đối tượng",
+			},
+		],
+		order: 3,
+		rightAns: "C",
+		userAns: "",
+		id: `quiz${2}`,
+	},
+	{
+		title: "Chức năng Separate có tác dụng gì?",
+		ans: [
+			{
+				answer: "A",
+				title: "Gộp nhiều đối tượng lại với nhau",
+			},
+			{
+				answer: "B",
+				title: "Tạo ra màu sắc",
+			},
+			{
+				answer: "C",
+				title: "Phép cắt bớt một đối tượng khỏi một đối tượng khác",
+			},
+			{
+				answer: "D",
+				title: "Chia tách các đối tượng",
+			},
+		],
+		order: 4,
+		rightAns: "D",
+		userAns: "",
+		id: `quiz${3}`,
+	},
+	{
+		title: "Solid Modeling sử dụng loại đối tượng nào để tạo mô hình 3D?",
+		ans: [
+			{
+				answer: "A",
+				title: "Đối tượng 2D",
+			},
+			{
+				answer: "B",
+				title: "Hình ảnh 2D",
+			},
+			{
+				answer: "C",
+				title: "Đối tượng 3D",
+			},
+			{
+				answer: "D",
+				title: "Màu sắc",
 			},
 		],
 		order: 5,
@@ -259,25 +387,25 @@ export const quizzes2 = [
 	},
 ];
 
-export const quizzes3 = [
+export const quizzes4 = [
 	{
-		title: "Trong Scratch, tọa độ (0,0) nằm ở đâu trên màn hình?",
+		title: "Terrain Editor trong Roblox Studio là gì?",
 		ans: [
 			{
 				answer: "A",
-				title: " Góc trên bên trái",
+				title: "Công cụ chỉnh sửa ánh sáng",
 			},
 			{
 				answer: "B",
-				title: "Góc dưới bên phải",
+				title: "Công cụ chỉnh sửa vị trí đối tượng",
 			},
 			{
 				answer: "C",
-				title: "Trung tâm màn hình",
+				title: "Công cụ chỉnh sửa đất đai và môi trường",
 			},
 			{
 				answer: "D",
-				title: "Góc trên bên phải",
+				title: "Công cụ chỉnh sửa màu sắc",
 			},
 		],
 		order: 1,
@@ -286,23 +414,151 @@ export const quizzes3 = [
 		id: `quiz${0}`,
 	},
 	{
-		title: "Lệnh nào sau đây cho phép di chuyển nhân vật đến một vị trí cụ thể trên màn hình?",
+		title: "Terrain Editor cho phép người dùng tạo ra những phần cảnh quan nào?",
 		ans: [
 			{
 				answer: "A",
-				title: " Di chuyển 10 bước",
+				title: "Biển cả",
 			},
 			{
 				answer: "B",
-				title: " Quay 15 độ",
+				title: "Rừng rậm",
 			},
 			{
 				answer: "C",
-				title: "Đi tới x: 0 y: 0",
+				title: "Núi non",
 			},
 			{
 				answer: "D",
-				title: "Đổi hướng 90 độ",
+				title: "Tất cả các phương án trên",
+			},
+		],
+		order: 2,
+		rightAns: "D",
+		userAns: "",
+		id: `quiz${1}`,
+	},
+	{
+		title: "Trong Roblox Studio, bạn có thể sử dụng Terrain Editor để tạo ra đối tượng nào?",
+		ans: [
+			{
+				answer: "A",
+				title: "Vật dụng",
+			},
+			{
+				answer: "B",
+				title: "Nhân vật",
+			},
+			{
+				answer: "C",
+				title: "Cảnh quan môi trường",
+			},
+			{
+				answer: "D",
+				title: "Tất cả phương án trên",
+			},
+		],
+		order: 3,
+		rightAns: "C",
+		userAns: "",
+		id: `quiz${2}`,
+	},
+	{
+		title: "Erosion trong Terrain Editor của Roblox Studio có tác dụng gì?",
+		ans: [
+			{
+				answer: "A",
+				title: "Làm mờ màu sắc",
+			},
+			{
+				answer: "B",
+				title: "Tạo độ bóng",
+			},
+			{
+				answer: "C",
+				title: "Làm cho đất đai trở nên mịn hơn",
+			},
+			{
+				answer: "D",
+				title: "Tạo các sự sụt lún",
+			},
+		],
+		order: 4,
+		rightAns: "D",
+		userAns: "",
+		id: `quiz${3}`,
+	},
+	{
+		title: "Terrain Editor cho phép bạn điều chỉnh độ cao của đất đai để làm gì?",
+		ans: [
+			{
+				answer: "A",
+				title: "Tạo độ bóng",
+			},
+			{
+				answer: "B",
+				title: "Tạo hiệu ứng âm thanh",
+			},
+			{
+				answer: "C",
+				title: "Tạo các mô hình 3D",
+			},
+			{
+				answer: "D",
+				title: "Tạo đất đai gồ ghềnh hoặc phẳng",
+			},
+		],
+		order: 5,
+		rightAns: "D",
+		userAns: "",
+		id: `quiz${4}`,
+	},
+];
+
+export const quizzes5 = [
+	{
+		title: "Effect trong Roblox Studio là gì?",
+		ans: [
+			{
+				answer: "A",
+				title: "Loại thức ăn",
+			},
+			{
+				answer: "B",
+				title: "Hiệu ứng hình ảnh hoặc âm thanh được áp dụng vào trò chơi",
+			},
+			{
+				answer: "C",
+				title: "Một dạng tiền tệ trong trò chơi",
+			},
+			{
+				answer: "D",
+				title: "Loại vật phẩm",
+			},
+		],
+		order: 1,
+		rightAns: "B",
+		userAns: "",
+		id: `quiz${0}`,
+	},
+	{
+		title: "Effect có thể làm gì trong Roblox Studio?",
+		ans: [
+			{
+				answer: "A",
+				title: "Tạo ra nhân vật",
+			},
+			{
+				answer: "B",
+				title: "Tạo ra các cảnh quan và đối tượng",
+			},
+			{
+				answer: "C",
+				title: "Tạo hiệu ứng hình ảnh và âm thanh đặc biệt",
+			},
+			{
+				answer: "D",
+				title: "Điều chỉnh ánh sáng trong trò chơi",
 			},
 		],
 		order: 2,
@@ -311,73 +567,73 @@ export const quizzes3 = [
 		id: `quiz${1}`,
 	},
 	{
-		title: "Khi bạn muốn nhân vật của mình di chuyển về phía trước, bạn sẽ sử dụng lệnh nào?",
+		title: "Có thể sử dụng Effect để thêm vào trò chơi những hiệu ứng như gì?",
 		ans: [
 			{
 				answer: "A",
-				title: "Di chuyển 10 bước",
+				title: "Tạo mô hình 3D",
 			},
 			{
 				answer: "B",
-				title: "Quay 15 độ",
+				title: "Làm mờ màu sắc",
 			},
 			{
 				answer: "C",
-				title: "Đi tới x: 0 y: 0",
+				title: "Tạo sương mù hoặc lửa cháy",
 			},
 			{
 				answer: "D",
-				title: "Đổi hướng 90 độ",
+				title: "Thay đổi kích thước đối tượng",
 			},
 		],
 		order: 3,
-		rightAns: "A",
+		rightAns: "C",
 		userAns: "",
 		id: `quiz${2}`,
 	},
 	{
-		title: "Lệnh “Đặt hướng về 90 độ” sẽ khiến nhân vật quay về hướng nào?",
+		title: "Bloom có tác dụng gì trong Roblox Studio?",
 		ans: [
 			{
 				answer: "A",
-				title: "Lên trên",
+				title: "Làm mờ hình ảnh",
 			},
 			{
 				answer: "B",
-				title: "Xuống dưới",
+				title: "Làm sáng hình ảnh để tạo hiệu ứng bóng sáng mạnh",
 			},
 			{
 				answer: "C",
-				title: "Sang phải",
+				title: "Tạo mô hình 3D",
 			},
 			{
 				answer: "D",
-				title: " Sang trái",
+				title: "Thay đổi kích thước đối tượng",
 			},
 		],
 		order: 4,
-		rightAns: "C",
+		rightAns: "B",
 		userAns: "",
 		id: `quiz${3}`,
 	},
 	{
-		title: "Khi bạn muốn nhân vật của mình di chuyển theo một hình vuông, bạn sẽ sử dụng lệnh nào?",
+		title: "Effect trong Roblox Studio có thể được sử dụng để làm gì trong Roblox Studio?",
 		ans: [
 			{
 				answer: "A",
-				title: "Di chuyển 10 bước",
+				title: "Hiển thị quảng cáo",
 			},
 			{
 				answer: "B",
-				title: "Lặp lại 4 [di chuyển 10 bước, quay phải 90 độ]",
+				title: "Tạo màu sắc đẹp",
 			},
 			{
 				answer: "C",
-				title: "",
+				title: "Tạo hiệu ứng đặc biệt",
 			},
 			{
 				answer: "D",
-				title: "",
+				title: "Tạo mô hình 3D",
 			},
 		],
 		order: 5,
@@ -395,98 +651,86 @@ const lessonsEmpty: CardProps = {
 export const lessons: CardProps[] = [
 	{
 		...lessonsEmpty,
-		title: "Bài 1: Làm quen với Scratch",
+		title: "Bài 1: Làm quen với Roblox Studio",
 		id: `lesson0`,
 		paramList: {
 			id: `lesson0`,
-			videoSource: "https://www.youtube.com/watch?v=QuZA24FEiIs&list=PLlJl1BR90sNE13WvpMX6oGjXzsZ90GKEM&index=8",
+			videoSource: "https://www.youtube.com/watch?v=OvylD-TMjeQ",
 		},
 		avata: {
-			uri: "https://www.icantech.vn/_next/image?url=https%3A%2F%2Fs3.icankid.io%2Ficantech%2Fcms%2F3_3x_960b0d2454.png&w=1920&q=100",
+			uri: "https://funix.edu.vn/wp-content/uploads/2023/02/khai-niem-ung-dung-roblox-studio.jpg",
 		},
 	},
 	{
 		...lessonsEmpty,
-		title: "Bài 2: Tạo nhân vật trong Scratch",
+		title: "Bài 2: Thuộc tính cơ bản của vật thể",
 		id: `lesson1`,
 		paramList: {
 			id: `lesson1`,
-			videoSource: "https://www.youtube.com/watch?v=6Sszrx9GAnE&t=50s",
+			videoSource: "https://www.youtube.com/watch?v=pKBs76uOdcQ",
 		},
 		avata: {
-			uri: "https://resources.mindx.edu.vn/uploads/images/ngon-ngu-lap-trinh-scratch-1687508459980-7347.jpg",
+			uri: "https://i.ytimg.com/vi/l20VZ5_nBrU/maxresdefault.jpg",
 		},
 	},
 	{
 		...lessonsEmpty,
-		title: "Bài 3: Tọa độ, di chuyển và hướng",
+		title: "Bài 3: Làm quen với Solid Modeling",
 		id: `lesson2`,
 		paramList: {
 			id: `lesson2`,
-			videoSource: "KDThNBO0vgw",
+			videoSource: "https://www.youtube.com/watch?v=2prQ2lFTzFE",
 		},
 		avata: {
-			uri: "https://resources.mindx.edu.vn/uploads/images/ngon-ngu-lap-trinh-scratch-1687508459980-7347.jpg",
+			uri: "https://prod.docsiteassets.roblox.com/assets/modeling/solid-modeling/Separate-Parts-To-Union.jpg",
 		},
 	},
 	{
 		...lessonsEmpty,
-		title: "Bài 4: Lệnh sự kiện",
+		title: "Bài 4: Solid Modeling Nâng cao",
 		id: `lesson3`,
 		paramList: {
 			id: `lesson3`,
-			videoSource: "KDThNBO0vgw",
+			videoSource: "https://www.youtube.com/watch?v=e1ebEyJN-Fo",
 		},
 		avata: {
-			uri: "https://resources.mindx.edu.vn/uploads/images/ngon-ngu-lap-trinh-scratch-1687508459980-7347.jpg",
+			uri: "https://prod.docsiteassets.roblox.com/assets/modeling/solid-modeling/Negated-Part-To-Union.jpg",
 		},
 	},
 	{
 		...lessonsEmpty,
-		title: "Bài 5: Lệnh điều kiện",
+		title: "Bài 5: Sử dụng Terrain Editor",
 		id: `lesson4`,
 		paramList: {
 			id: `lesson4`,
-			videoSource: "KDThNBO0vgw",
+			videoSource: "https://www.youtube.com/watch?v=JVIC1bVeddA",
 		},
 		avata: {
-			uri: "https://resources.mindx.edu.vn/uploads/images/ngon-ngu-lap-trinh-scratch-1687508459980-7347.jpg",
+			uri: "https://i.ytimg.com/vi/hMxSMJSXJTo/maxresdefault.jpg",
 		},
 	},
 	{
 		...lessonsEmpty,
-		title: "Bài 5: Lệnh điều kiện",
-		id: `lesson4`,
-		paramList: {
-			id: `lesson4`,
-			videoSource: "KDThNBO0vgw",
-		},
-		avata: {
-			uri: "https://resources.mindx.edu.vn/uploads/images/ngon-ngu-lap-trinh-scratch-1687508459980-7347.jpg",
-		},
-	},
-	{
-		...lessonsEmpty,
-		title: "Bài 6: Lệnh lặp",
+		title: "Bài 6: Ứng dụng Effect trong thiết kế",
 		id: `lesson5`,
 		paramList: {
 			id: `lesson5`,
-			videoSource: "KDThNBO0vgw",
+			videoSource: "https://www.youtube.com/watch?v=ftIjeYFdfPk",
 		},
 		avata: {
-			uri: "https://resources.mindx.edu.vn/uploads/images/ngon-ngu-lap-trinh-scratch-1687508459980-7347.jpg",
+			uri: "https://i.ytimg.com/vi/DQrlh_PpaAA/maxresdefault.jpg",
 		},
 	},
 	{
 		...lessonsEmpty,
-		title: "Bài 7: Lệnh cảm biến",
+		title: "Bài 7: Giới thiệu về các Plugin hỗ trợ",
 		id: `lesson6`,
 		paramList: {
 			id: `lesson6`,
-			videoSource: "KDThNBO0vgw",
+			videoSource: "https://www.youtube.com/watch?v=M6Q7Pq9ZcNQ",
 		},
 		avata: {
-			uri: "https://resources.mindx.edu.vn/uploads/images/ngon-ngu-lap-trinh-scratch-1687508459980-7347.jpg",
+			uri: "https://i.ytimg.com/vi/Rou1Wt9NOww/maxresdefault.jpg",
 		},
 	},
 ];
@@ -502,7 +746,7 @@ const quizEmty: CardProps = {
 export const quizz = [
 	{
 		...quizEmty,
-		title: "Giới thiệu về Scratch",
+		title: "Câu hỏi về Roblox Studio",
 		id: `quiz0`,
 		paramList: {
 			id: `quiz0`,
@@ -514,7 +758,7 @@ export const quizz = [
 	},
 	{
 		...quizEmty,
-		title: "Câu hỏi ôn tập bài 2",
+		title: "Câu hỏi về thuộc tính của vật thể",
 		id: `quiz1`,
 		paramList: {
 			id: `quiz1`,
@@ -522,6 +766,42 @@ export const quizz = [
 			quizzes: quizzes2,
 		},
 		quizzes: quizzes2,
+		isComplete: false,
+	},
+	{
+		...quizEmty,
+		title: "Câu hỏi về Solid Model",
+		id: `quiz2`,
+		paramList: {
+			id: `quiz2`,
+			order: 2,
+			quizzes: quizzes3,
+		},
+		quizzes: quizzes3,
+		isComplete: false,
+	},
+	{
+		...quizEmty,
+		title: "Câu hỏi về Terrain Editor",
+		id: `quiz3`,
+		paramList: {
+			id: `quiz3`,
+			order: 2,
+			quizzes: quizzes4,
+		},
+		quizzes: quizzes4,
+		isComplete: false,
+	},
+	{
+		...quizEmty,
+		title: "Câu hỏi về Effect trong Roblox Studio",
+		id: `quiz4`,
+		paramList: {
+			id: `quiz4`,
+			order: 2,
+			quizzes: quizzes5,
+		},
+		quizzes: quizzes5,
 		isComplete: false,
 	},
 ];
